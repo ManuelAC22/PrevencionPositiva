325c7e18ba8ed617ac5ac42248cb8d156d89e8eb
Manuel Alcalde <mac2291998@gmail.com>
2018-09-15 07:56:38 -0500

    commit 3.0 alcalde

--- app/src/main/java/com/prevencionpositiva/MainActivity.java
+++ app/src/main/java/com/prevencionpositiva/MainActivity.java
@@ -6,23 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_agricola;
+    ImageButton btn_agricola;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Holi", Toast.LENGTH_SHORT).show();
             }
         });
-
+*/
         btn_agricola = findViewById(R.id.btn_agricola);
         btn_agricola.setOnClickListener(new View.OnClickListener() {
             @Override
