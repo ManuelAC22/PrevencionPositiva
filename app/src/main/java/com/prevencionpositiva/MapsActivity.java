620a379da1933dcb6b0992899dd7f48e875adeba
Manuel Alcalde <mac2291998@gmail.com>
2018-09-15 03:36:12 -0500

    commit 1.0 alcalde

--- app/src/main/java/com/prevencionpositiva/MapsActivity.java
+++ app/src/main/java/com/prevencionpositiva/MapsActivity.java
@@ -1,18 +1,45 @@
 package com.prevencionpositiva;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    String direccion;
+    String mensaje;
+    double lat = 0.0;
+    double log = 0.0;
+    LatLng latLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +51,108 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.maps_positiva));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            if (!success) {
+                Log.e("", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("", "Can't find style. Error: ", e);
+        }
+
+        miUbicacion();
+    }
+
+    public void locationStart(){
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if(!gpsEnabled){
+            Intent settingIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingIntent);
+        }
+    }
+
+    public void setLocation(Location loc){
+        if(loc.getLatitude()!=0.0 && loc.getLongitude()!=0.0){
+            try{
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(),loc.getLongitude(),1);
+                if(!list.isEmpty()){
+                    Address dircalle = list.get(0);
+                    direccion = (dircalle.getAddressLine(0));
+                }
+            } catch (IOException e) {
+                Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void agregarMarcador(double lat, double log) {
+        LatLng asd = new LatLng(lat, log);
+        Marker MiUbicación = mMap.addMarker(new MarkerOptions().position(asd).title("Mi Ubicación").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(asd, 15));
+        UiSettings ui = mMap.getUiSettings();
+        ui.setZoomControlsEnabled(true);
+    }
+
+    public void actulizarMarker(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            log = location.getLongitude();
+            agregarMarcador(lat, log);
+            Log.d("asd",lat+"  "+log);
+        }
+
+    }
+
+    LocationListener locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            //actulizarMarker(location);
+            setLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+            mensaje = ("GPS Activado");
+            Mensaje();
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+            mensaje = ("GPS Desactivado");
+            locationStart();
+            Mensaje();
+        }
+    };
+
+    private void Mensaje() {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+    public void miUbicacion() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION,},101);
+            return;
+        }else{
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+            actulizarMarker(location);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1200,0,locListener);
+        }
     }
 }
